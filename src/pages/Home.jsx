69a836980fa7ad315268e210a86fdb966e3f2dfe
Chimi <chimiyuden19@gmail.com>
2025-06-16 14:20:57 +0600

    changed the home details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,6 @@ const Home = () => {
       <section className="intro-section">
         <div className="intro-content">
           <div className="cta-container">
-            <a href="#projects" className="cta-button">
-              View My Work
-              <svg className="arrow-icon" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7" />
-              </svg>
-            </a>
             <a href="#contact" className="cta-button secondary">
               Contact Me: 123456789
             </a>
