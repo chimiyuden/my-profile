463f63c640d71e395828607362ad83e9260e9a2b
hirono <chimiyuden19@gmail.com>
2025-06-16 20:57:31 +0600

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.js (With React Router)
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -6,14 +5,14 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import "./App.css";
-import Home from "./pages/Home";
+import Hero from "./components/Hero";
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Hero />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
